c9fa463039d10c9ba02d8b62e2423c443acab0b4
pragna-1925 <152945098+pragna-1925@users.noreply.github.com>
2023-12-05 09:28:52 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,36 @@
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["abc"]
+   var passwordArray = ["123"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   window.location = "home.html"
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   window.location = "home.html"
+   count--;
+}
+}
